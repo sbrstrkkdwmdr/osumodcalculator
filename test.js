94bb7d68134631b75c3cf5895dc39a49e4647494
SaberStrike <71379681+sbrstrkkdwmdr@users.noreply.github.com>
2022-07-24 12:31:06 +1000

    e

--- test.js
+++ test.js
@@ -1,3 +1,5 @@
 const osumodcalc = require('./index.cjs')
 
-console.log(osumodcalc.csFromRadius(27.524))
+console.log(osumodcalc.OrderMods('HDDT,HR 4k 5k'))
+console.log(osumodcalc.shortModName('hidden doubletime among us'))
+console.log(osumodcalc.longModName('HDDTHR'))
