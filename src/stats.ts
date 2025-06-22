9c37566fb178021c75d65b8235241459b12e2ddc
SaberStrike <71379681+sbrstrkkdwmdr@users.noreply.github.com>
2025-06-22 14:12:05 +1000

    wip

--- src/stats.ts
+++ src/stats.ts
@@ -2,10 +2,78 @@ import { types } from ".";
 
 /**
  * 
+ * @param od overall difficulty / accuracy
+ * @returns hitwindow values in milliseconds
+ */
+export function ToMsOd(od: number) {
+    const rangeobj = {
+        hitwindow_300: 79 - (od * 6) + 0.5,
+        hitwindow_100: 139 - (od * 8) + 0.5,
+        hitwindow_50: 199 - (od * 10) + 0.5,
+    };
+    return rangeobj;
+}
+/**
+ * 
+ * @param ar approach rate
+ * @returns approach rate converted to milliseconds
+ */
+function ToMsAr(ar: number) {
+    const ogtoms = ar > 5 ? 1200 - (((ar - 5) * 10) * 15) : 1800 - (((ar) * 10) * 12);
+    return ogtoms;
+}
+/**
+ * 
+ * @param hitwindow300 300 hit window in milliseconds
+ * @param hitwindow100 100 hit window in milliseconds
+ * @param hitwindow50 50 hit window in milliseconds
+ * @info set a value to a string to ignore
+ * @returns od (overall difficulty)
+ */
+export function FromMsOd(hitwindow300: number, hitwindow100?: number, hitwindow50?: number) {
+    let od: string = 'NaN';
+    if (!isNaN(hitwindow300)) {
+        od = ((79.5 - hitwindow300) / 6).toFixed(2);
+    }
+    else if (hitwindow100 && !isNaN(hitwindow100)) {
+        od = ((139.5 - hitwindow100) / 8).toFixed(2);
+    }
+    else if (hitwindow50 && !isNaN(hitwindow50)) {
+        od = ((199.5 - hitwindow50) / 10).toFixed(2);
+    }
+    if (+od > 11) {
+        od = '11';
+    }
+    return ToMsOd(+od);
+}
+/**
+ * 
+ * @param ms milliseconds
+ * @returns ar (approach rate)
+ */
+export function FromMsAr(ms: number): types.ApproachRate {
+    let ar: number;
+    if (ms < 300) {
+        ar = 11;
+    }
+    else if (ms < 1200) {
+        ar = Math.round((11 - (ms - 300) / 150) * 100) / 100;
+    }
+    else {
+        ar = Math.round((5 - (ms - 1200) / 120) * 100) / 100;
+    }
+    return {
+        ar,
+        ms,
+    };
+}
+
+/**
+ * 
  * @param ar approach rate
  * @returns approach rate if the double time mod is applied
  */
-export function DoubleTimeAR(ar: number) {
+export function ToDtAr(ar: number) {
     /*     if (ar > 5) {
             ms = 200 + (11 - ar) * 100;
         }
@@ -35,7 +103,7 @@ export function DoubleTimeAR(ar: number) {
  * @param ar approach rate
  * @returns approach rate if the half time mod is applied
  */
-export function HalfTimeAR(ar: number) {
+export function ToHtAr(ar: number) {
     let newAR: number;
 
     const ogtoms = ar > 5 ? 1200 - ((ar - 5) * 10 * 15) : 1800 - ((ar * 10) * 12);
@@ -56,82 +124,13 @@ export function HalfTimeAR(ar: number) {
     };
     return arobj;
 }
-/**
- * 
- * @param od overall difficulty / accuracy
- * @returns hitwindow values in milliseconds
- */
-export function ODtoms(od: number) {
-    const rangeobj = {
-        hitwindow_300: 79 - (od * 6) + 0.5,
-        hitwindow_100: 139 - (od * 8) + 0.5,
-        hitwindow_50: 199 - (od * 10) + 0.5,
-    };
-    return rangeobj;
-}
-/**
- * 
- * @param ar approach rate
- * @returns approach rate converted to milliseconds
- */
-function ARtoms(ar: number) {
-    const ogtoms = ar > 5 ? 1200 - (((ar - 5) * 10) * 15) : 1800 - (((ar) * 10) * 12);
-    return ogtoms;
-}
-/**
- * 
- * @param hitwindow300 300 hit window in milliseconds
- * @param hitwindow100 100 hit window in milliseconds
- * @param hitwindow50 50 hit window in milliseconds
- * @info set a value to a string to ignore
- * @returns od (overall difficulty)
- */
-export function msToOD(hitwindow300: number, hitwindow100?: number, hitwindow50?: number) {
-    let od: string = 'NaN';
-    if (!isNaN(hitwindow300)) {
-        od = ((79.5 - hitwindow300) / 6).toFixed(2);
-    }
-    else if (hitwindow100 && !isNaN(hitwindow100)) {
-        od = ((139.5 - hitwindow100) / 8).toFixed(2);
-    }
-    else if (hitwindow50 && !isNaN(hitwindow50)) {
-        od = ((199.5 - hitwindow50) / 10).toFixed(2);
-    }
-    if (+od > 11) {
-        od = '11';
-    }
-    return ODtoms(+od);
-}
-/**
- * 
- * @param ms milliseconds
- * @returns ar (approach rate)
- */
-export function msToAR(ms: number): types.ApproachRate {
-    let ar: number;
-    if (ms < 300) {
-        ar = 11;
-    }
-    else if (ms < 1200) {
-        ar = Math.round((11 - (ms - 300) / 150) * 100) / 100;
-    }
-    else {
-        ar = Math.round((5 - (ms - 1200) / 120) * 100) / 100;
-    }
-    return {
-        ar,
-        ms,
-    };
-}
-
-
 
 /**
  * 
  * @param od overall difficulty / accuracy
  * @returns ms values for the od hitwindows and converts to double time
  */
-export function odDT(od: number) {
+export function ToDtOd(od: number) {
     const range300 = ((79 - (od * 6) + 0.5) * 2 / 3) + 0.33;
     const odobj = {
         hitwindow_300: range300,
@@ -147,7 +146,7 @@ export function odDT(od: number) {
  * @param od overall difficulty / accuracy
  * @returns ms values for the od hitwindows and converts to half time
  */
-export function odHT(od: number) {
+export function ToHtOd(od: number) {
     const range300 = ((79 - (od * 6) + 0.5) * 4 / 3) + 0.66;
     const odobj = {
         hitwindow_300: range300,
@@ -157,6 +156,8 @@ export function odHT(od: number) {
 
     return odobj;
 }
+
+
 
 /**
  * 
@@ -210,6 +211,45 @@ export function csFromRadius(radius: number) {
     return (5000 / 8104533921) * radius ** 2 - (1808448550 / 8104533921) * radius + (8582285633270972 / 706821088118109);
 }
 
+export function speed(i: 'DT' | 'HT' | number, stats: {
+    ar: number,
+    od: number,
+    bpm: number,
+    song_length: number,
+}) {
+    if(!i){
+        i = 1.0
+    }
+    if (typeof i == 'string') {
+        switch (i) {
+            case "DT":
+                i = 1.5;
+                break;
+            case "HT":
+                i = 0.75;
+                break;
+        }
+    }
+    const modAr = ToMsAr(stats.ar) / i;
+    const modOd = ToMsOd(stats.od);
+    modOd.hitwindow_300 /= i;
+    modOd.hitwindow_100 /= i;
+    modOd.hitwindow_50 /= i;
+
+    const modBpm = stats.bpm / i;
+    const modSl = stats.song_length / i;
+    return {
+        ar: FromMsAr(modAr),
+        od: FromMsOd(modOd.hitwindow_300),
+        bpm: modBpm,
+        song_length: modSl,
+        extra: {
+            arMs: modAr,
+            odMs: modOd,
+        }
+    };
+}
+
 export function modded(stats: {
     cs: number,
     ar: number,
@@ -232,13 +272,13 @@ export function modded(stats: {
                 song_length: stats.song_length,
                 extra: {
                     csRadius: csToRadius(stats.cs),
-                    arMs: ARtoms(stats.ar),
-                    odMs: ODtoms(stats.od),
+                    arMs: ToMsAr(stats.ar),
+                    odMs: ToMsOd(stats.od),
                 }
             };
         }
-        const modAr = ARtoms(stats.ar) / customSpeed;
-        const modOd = ODtoms(stats.od);
+        const modAr = ToMsAr(stats.ar) / customSpeed;
+        const modOd = ToMsOd(stats.od);
         modOd.hitwindow_300 /= customSpeed;
         modOd.hitwindow_100 /= customSpeed;
         modOd.hitwindow_50 /= customSpeed;
@@ -247,8 +287,8 @@ export function modded(stats: {
         const modSl = stats.song_length / customSpeed;
         return {
             cs: stats.cs,
-            ar: msToAR(modAr),
-            od: msToOD(modOd.hitwindow_300),
+            ar: FromMsAr(modAr),
+            od: FromMsOd(modOd.hitwindow_300),
             hp: stats.hp,
             bpm: modBpm,
             song_length: modSl,
@@ -260,8 +300,8 @@ export function modded(stats: {
         };
     }
     let modCs = stats.cs;
-    let modArMs = ARtoms(stats.ar);
-    let modOdMs = ODtoms(stats.od);
+    let modArMs = ToMsAr(stats.ar);
+    let modOdMs = ToMsOd(stats.od);
     let modHp = stats.hp;
     let modBpm = stats.bpm;
     let modSl = stats.song_length;
@@ -271,13 +311,13 @@ export function modded(stats: {
 
         if (mods.includes('HR')) {
             modCs *= 1.3;
-            modArMs = ARtoms(stats.ar * 1.4);
-            modOdMs = ODtoms(stats.od * 1.4);
+            modArMs = ToMsAr(stats.ar * 1.4);
+            modOdMs = ToMsOd(stats.od * 1.4);
             modHp *= 1.4;
         } else if (mods.includes('EZ')) {
             modCs /= 2;
-            modArMs = ARtoms(stats.ar / 2);
-            modOdMs = ODtoms(stats.od / 2);
+            modArMs = ToMsAr(stats.ar / 2);
+            modOdMs = ToMsOd(stats.od / 2);
             modHp /= 2;
         }
 
@@ -291,13 +331,13 @@ export function modded(stats: {
 
         if (modacrs.includes('HR')) {
             modCs *= 1.3;
-            modArMs = ARtoms(stats.ar * 1.4);
-            modOdMs = ODtoms(stats.od * 1.4);
+            modArMs = ToMsAr(stats.ar * 1.4);
+            modOdMs = ToMsOd(stats.od * 1.4);
             modHp *= 1.4;
         } else if (modacrs.includes('EZ')) {
             modCs /= 2;
-            modArMs = ARtoms(stats.ar / 2);
-            modOdMs = ODtoms(stats.od / 2);
+            modArMs = ToMsAr(stats.ar / 2);
+            modOdMs = ToMsOd(stats.od / 2);
             modHp /= 2;
         }
 
@@ -326,8 +366,8 @@ export function modded(stats: {
 
     return {
         cs: stats.cs,
-        ar: msToAR(modArMs),
-        od: msToOD(modOdMs.hitwindow_300),
+        ar: FromMsAr(modArMs),
+        od: FromMsOd(modOdMs.hitwindow_300),
         hp: stats.hp,
         bpm: modBpm,
         song_length: modSl,
